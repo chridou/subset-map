75c7d341afa911bba22ac08ee9b46721ffa1f311
chridou <chridou@users.noreply.github.com>
2018-04-29 20:31:59 +0200

    implement find

--- src/lib.rs
+++ src/lib.rs
@@ -1,168 +1,138 @@
-pub type Nodes<I, P> = Vec<Node<I, P>>;
+type Nodes<I, P> = Vec<SubsetMapNode<I, P>>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Root<I, P> {
+pub struct SubsetMap<I, P> {
     pub nodes: Nodes<I, P>,
 }
 
-impl<I, P> Root<I, P>
-where
-    I: Clone,
-    P: Default,
-{
-    pub fn new(ids: &[I]) -> Root<I, P> {
-        init_root(ids, &|| P::default())
-    }
-}
-
-impl<I, P> Root<I, P>
+impl<I, P> SubsetMap<I, P>
 where
     I: Clone,
 {
-    pub fn init_with<F>(ids: &[I], init: F) -> Root<I, P>
+    pub fn new<F>(ids: &[I], init: F) -> SubsetMap<I, P>
     where
-        F: Fn() -> P,
+        F: Fn(&[I]) -> P,
     {
         init_root(ids, &init)
     }
-}
 
-impl<I, P> Root<I, P> {
+    pub fn with_value<F>(ids: &[I], init: F) -> SubsetMap<I, P>
+    where
+        F: Fn() -> P,
+    {
+        init_root(ids, &|_| init())
+    }
+
+    pub fn with_defaults(ids: &[I]) -> SubsetMap<I, P>
+    where
+        P: Default,
+    {
+        init_root(ids, &|_| P::default())
+    }
+
     pub fn is_empty(&self) -> bool {
         self.nodes.is_empty()
     }
 
-    pub fn combinations(&self) -> Vec<Vec<&I>> {
-        unimplemented!()
+    pub fn lookup(&self, subset: &[I]) -> Option<&P>
+    where
+        I: Eq,
+    {
+        match self.find(subset) {
+            Some(MatchQuality::Perfect(p)) => Some(p),
+            _ => None,
+        }
     }
 
-    pub fn iter(&self) -> Iter<I, P> {
-        unimplemented!()
+    pub fn find(&self, subset: &[I]) -> Option<MatchQuality<I, P>>
+    where
+        I: Eq,
+    {
+        if subset.is_empty() {
+            None
+        } else {
+            let mut skipped = Vec::with_capacity(subset.len());
+            find(subset, &self.nodes, &mut skipped);
+        }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Node<I, P> {
+pub struct SubsetMapNode<I, P> {
     pub id: I,
     pub payload: P,
     pub nodes: Nodes<I, P>,
 }
 
-pub trait NodeLike {
-    type I;
-    type P;
-    fn nodes(&self) -> &[Node<Self::I, Self::P>];
+pub enum MatchQuality<'a, 'b, I: 'a, P: 'b> {
+    Perfect(&'b P),
+    Nearby(&'b P, Vec<&'a I>),
 }
 
-impl<I, P> NodeLike for Root<I, P> {
-    type I = I;
-    type P = P;
-    fn nodes(&self) -> &[Node<Self::I, Self::P>] {
-        &self.nodes
-    }
-}
-
-impl<I, P> NodeLike for Node<I, P> {
-    type I = I;
-    type P = P;
-    fn nodes(&self) -> &[Node<Self::I, Self::P>] {
-        &self.nodes
-    }
-}
-
-pub struct Iter<'a, I, P>
-where
-    I: 'a,
-    P: 'a,
-{
-    root: &'a Root<I, P>,
-    root_index: usize,
-    stack: Vec<(usize, &'a Node<I, P>)>,
-    ids: Vec<I>,
-}
-
-impl<'a, I, P> Iter<'a, I, P> {
-    pub fn new(root: &'a Root<I, P>) -> Self {
-        Iter {
-            root,
-            root_index: 0,
-            stack: Vec::with_capacity(root.nodes.len()),
-            ids: Vec::with_capacity(root.nodes.len()),
+impl<'a, 'b, I, P> MatchQuality<'a, 'b, I, P> {
+    pub fn value(&self) -> &P {
+        match *self {
+            MatchQuality::Perfect(p) => p,
+            MatchQuality::Nearby(p, _) => p,
         }
     }
 }
 
-impl<'a, I, P> Iterator for Iter<'a, I, P>
+fn find<'a, 'b, I, P>(subset: &[I], nodes: &[SubsetMapNode<I, P>])
 where
-    I: 'a,
-    P: 'a,
+    I: Eq,
 {
-    type Item = (&'a [I], &'a P);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.root_index < self.root.nodes.len() {
-            if self.stack.is_empty() {
-                self.stack.push((0, &self.root.nodes[self.root_index]));
-            }
-            let &mut (current_index, current_node) = self.stack.last_mut().unwrap();
-            let result = if current_index < current_node.nodes.len() {
-                self.ids.push(current_node.id);
-                Some((&*self.ids, &current_node.payload))
-            } else {
-                self.stack.pop();
-                if self.stack.is_empty() {
-                    self.root_index += 1;
-                }
-                None
-            };
-            result
-        } else {
-            None
-        }
-    }
+    unimplemented!()
 }
 
-fn init_root<I, P, F>(ids: &[I], init_with: &F) -> Root<I, P>
+fn init_root<I, P, F>(ids: &[I], init_with: &F) -> SubsetMap<I, P>
 where
     I: Clone,
-    F: Fn() -> P,
+    F: Fn(&[I]) -> P,
 {
     let nodes: Nodes<_, _> = (0..ids.len())
         .map(|fixed| {
-            let mut node = Node {
-                id: ids[fixed].clone(),
-                payload: init_with(),
+            let id = ids[fixed].clone();
+            let payload = init_with(&ids[fixed..fixed + 1]);
+            let mut node = SubsetMapNode {
+                id,
+                payload,
                 nodes: Vec::new(),
             };
-            init_node(ids, fixed, &mut node, init_with);
+            init_node(ids, fixed, fixed, &mut node, init_with);
             node
         })
         .collect();
-    Root { nodes }
+    SubsetMap { nodes }
 }
 
-fn init_node<I, P, F>(ids: &[I], fixed: usize, into: &mut Node<I, P>, init_with: &F)
-where
+fn init_node<I, P, F>(
+    ids: &[I],
+    initial: usize,
+    fixed: usize,
+    into: &mut SubsetMapNode<I, P>,
+    init_with: &F,
+) where
     I: Clone,
-    F: Fn() -> P,
+    F: Fn(&[I]) -> P,
 {
     for fixed in fixed + 1..ids.len() {
-        let mut node = Node {
+        let mut node = SubsetMapNode {
             id: ids[fixed].clone(),
-            payload: init_with(),
+            payload: init_with(&ids[initial..fixed]),
             nodes: Vec::new(),
         };
-        init_node(ids, fixed, &mut node, init_with);
+        init_node(ids, initial, fixed, &mut node, init_with);
         into.nodes.push(node);
     }
 }
 
-impl<I, P> Default for Root<I, P>
+impl<I, P> Default for SubsetMap<I, P>
 where
     I: Ord,
 {
     fn default() -> Self {
-        Root {
+        SubsetMap {
             nodes: Default::default(),
         }
     }
@@ -173,17 +143,16 @@ mod tests {
     use super::*;
     #[test]
     fn create_empty() {
-        let sample = Root::<u32, ()>::default();
+        let sample = SubsetMap::<u32, ()>::default();
 
         assert!(sample.is_empty());
     }
 
     #[test]
     fn bogus() {
-        let sample = Root::<u32, ()>::default();
-        let expected = Root::new(&[1, 2, 3]);
+        let sample = SubsetMap::<_, ()>::with_defaults(&[]);
+        let expected = SubsetMap::with_defaults(&[1, 2, 3]);
 
         assert_eq!(sample, expected);
     }
-
 }
